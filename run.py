2c7922e318418c2837048e3cc058a20c9f8e51fc
thuhoai05 <thuhoai05102005@gmail.com>
2026-03-24 23:31:36 +0700

    file này k cần dùng đến

--- run.py
+++ run.py
@@ -1,8 +1,1 @@
-# run.py
 
-import subprocess
-
-subprocess.run(["python", "build_index.py"])
-subprocess.run(["python", "preprocess_for_training.py"])
-subprocess.run(["python", "train.py"])
-subprocess.run(["python", "app.py"])
